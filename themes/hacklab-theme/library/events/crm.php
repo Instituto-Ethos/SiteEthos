8cff4d32be761ee524c8aa2c6fe74303d1c9e954
leopiccionia <leopiccionia@gmail.com>
2025-05-26 16:35:20 -0300

    Map accounts and contacts for events registration

--- themes/hacklab-theme/library/events/crm.php
+++ themes/hacklab-theme/library/events/crm.php
@@ -52,22 +52,54 @@ function create_registration_contact (array $params) {
     $last_name = implode(' ', $name_parts);
 
     $attributes = [
-        'emailaddress1' => trim($params['email']),
-        'firstname' => $first_name,
-        'fullname' => $full_name,
-        'fut_pl_area' => trim($params['area']),
+        'emailaddress1'           => trim($params['email']),
+        'firstname'               => $first_name,
+        'fullname'                => $full_name,
+        'fut_pl_area'             => trim($params['area']),
         'fut_pl_nivelhierarquico' => trim($params['nivel_hierarquico']),
-        'fut_st_cpf' => trim($params['cpf']),
-        'jobtitle' => trim($params['cargo']),
-        'lastname' => $last_name,
-        'originatingleadid' => create_crm_reference('lead', $lead_id),
-        'telephone1' => trim($params['telefone']),
-        'yomifirstname' => $first_name,
-        'yomifullname' => $full_name,
-        'yomilastname' => $last_name,
+        'fut_st_cpf'              => format_cpf(trim($params['cpf'])),
+        'jobtitle'                => trim($params['cargo']),
+        'lastname'                => $last_name,
+        'originatingleadid'       => create_crm_reference('lead', $lead_id),
+        'telephone1'              => trim($params['telefone']),
+        'yomifirstname'           => $first_name,
+        'yomifullname'            => $full_name,
+        'yomilastname'            => $last_name,
     ];
 
     return create_crm_entity('contact', $attributes);
+}
+
+function create_registration_lead (array $params) {
+    $systemuser = get_option('systemuser');
+
+    $cnpj = trim($params['cnpj']);
+    $company_name = trim($params['nome_fantasia']);
+
+    $full_name = trim($params['nome_completo']);
+    $name_parts = explode(' ',  $full_name);
+    $first_name = $name_parts[0];
+    unset($name_parts[0]);
+    $last_name = implode(' ', $name_parts);
+
+    $attributes = [
+        'ownerid' => create_crm_reference('systemuser', $systemuser),
+        'companyname'                => $company_name,
+        'firstname'                  => $company_name,
+        'fullname'                   => $company_name,
+        'fut_st_cnpj'                => format_cnpj($cnpj),
+        'fut_st_cnpjsemmascara'      => $cnpj,
+        'fut_st_nome'                => $first_name,
+        'fut_st_nomecompleto'        => $full_name,
+        'fut_st_nomefantasiaempresa' => $company_name,
+        'fut_st_sobrenome'           => $last_name,
+        'leadsourcecode'             => 4, // Eventos
+        'yomifirstname'              => $first_name,
+        'yomifullname'               => $company_name,
+        'yomilastname'               => $last_name,
+    ];
+
+    return create_crm_entity('lead', $attributes);
 }
 
 function get_event_registrations (int $post_id) {
@@ -87,13 +119,8 @@ function get_event_registrations (int $post_id) {
     return $account_ids;
 }
 
-function get_registration_lead (array $params): string {
-    //@TODO
-    return '';
-}
-
 function get_registration_contact (array $params): string {
-    // Case 1. Retrieve UUID from current user's metadata
+    // Case 1. Retrieve UUID from current user's data
     if (($user_id = get_current_user_id()) && ($contact_id = get_user_meta($user_id, '_ethos_crm_contact_id', true))) {
         return $contact_id;
     }
@@ -120,6 +147,36 @@ function get_registration_contact (array $params): string {
 
     // Case 4. If contact does not exist, create it, and return its UUID
     return create_registration_contact($params);
+}
+
+function get_registration_lead (array $params): string {
+    // Case 1. Retrieve UUID from current user's organization
+    if (($post_id = get_organization_by_user()) && ($lead_id = get_post_meta($post_id, '_ethos_crm_lead_id', true))) {
+        return $lead_id;
+    }
+
+    // Case 2. Retrieve UUID from other WordPress organizations
+    $posts = get_posts([
+        'meta_query' => [
+            [ 'key' => 'cnpj', 'value' => $params['cnpj'] ],
+        ],
+    ]);
+    if (!empty($posts) && ($lead_id = get_post_meta($posts[0]->ID, '_ethos_crm_lead_id', true))) {
+        return $lead_id;
+    }
+
+    // Case 3. Retrieve UUID directly from CRM leads
+    $leads = get_crm_entities('lead', [
+        'filters' => [
+            'fut_st_cnpjsemmascara' => $params['cnpj'],
+        ],
+    ]);
+    if (!empty($leads->Entities)) {
+        return $leads->Entities[0]->Id;
+    }
+
+    // Case 4. If lead does not exist, create it, and return its UUID
+    return create_registration_lead($params);
 }
 
 function register_for_event (array $params) {
