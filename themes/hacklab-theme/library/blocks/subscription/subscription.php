1039471862cc5f6898c01a716d509ac0a6a6733f
Everaldo Matias <matiaseveraldo@gmail.com>
2025-09-17 15:55:45 -0300

    Allows the manager to see subscriptions of the associate.

--- themes/hacklab-theme/library/blocks/subscription/subscription.php
+++ themes/hacklab-theme/library/blocks/subscription/subscription.php
@@ -26,6 +26,19 @@ function get_subscription_data ($attributes): array {
 function render_subscription_callback ($attributes) {
     $user_id = get_current_user_id();
 
+    /**
+     * Checks if the current user has permission to edit other associates.
+     * If so, retrieves the organization ID from the GET request and validates its post type.
+     * If the organization is valid, fetches the user ID of the organization author.
+     */
+    if ( current_user_can( 'edit_others_associates' ) ) {
+        $organization_id = isset( $_GET['organization'] ) ? intval( $_GET['organization'] ) : 0;
+
+        if ( $organization_id && get_post_type( $organization_id ) === 'organizacao' ) {
+            $user_id = get_post_field( 'post_author', $organization_id );
+        }
+    }
+
     $organization = get_organization_by_user($user_id);
 
     if (empty($organization)) {
