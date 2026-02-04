104de1275b2d796ad3143a94a99ae03dd7674a86
leopiccionia <leopiccionia@gmail.com>
2026-02-04 14:44:57 -0300

    Just display duplicate contacts inside the same account

--- themes/hacklab-theme/library/scripts/find-duplicates.php
+++ themes/hacklab-theme/library/scripts/find-duplicates.php
@@ -13,6 +13,8 @@ function cli_find_duplicates () {
     $users_map = [];
 
     foreach ($users as $user) {
+        $company_id = get_user_meta($user->ID, '_ethos_crm_account_id', true);
+
         $email = get_user_meta($user->ID, '_ethos_crm:emailaddress1', true);
         if (empty($email)) {
             continue;
@@ -27,37 +29,40 @@ function cli_find_duplicates () {
         $email_user_parts = explode('+', $email_parts[0]);
         $normalized_email = $email_user_parts[0] . '@' . $email_parts[1];
 
-        if (!isset($users_map[$normalized_email])) {
-            $users_map[$normalized_email] = [];
+        if (!isset($users_map[$company_id][$normalized_email])) {
+            if (!isset($users_map[$company_id])) {
+                $users_map[$company_id] = [];
+            }
+            $users_map[$company_id][$normalized_email] = [];
         }
-        $users_map[$normalized_email][] = $user->ID;
+        $users_map[$company_id][$normalized_email][] = $user->ID;
     }
 
-    foreach ($users_map as $email => $user_ids) {
-        $count = count($user_ids);
-        if ($count > 1) {
-            \WP_CLI::log("Found $count users with email = $email\n");
-
-            foreach ($user_ids as $user_id) {
-                $user = get_user_by('ID', $user_id);
-                $user_meta = get_user_meta($user_id);
-                $user_slug = $user->user_login;
-
-                $company_id = get_user_meta($user_id, '_ethos_crm_account_id', true);
+    foreach ($users_map as $company_id => $users_by_company) {
+        foreach ($users_by_company as $email => $user_ids) {
+            $count = count($user_ids);
+            if ($count > 1) {
                 $company_post_id = \ethos\crm\get_post_id_by_account($company_id);
                 $company_name = get_the_title($company_post_id);
 
-                ksort($user_meta);
+                \WP_CLI::log("Found $count users with email = \"$email\" on company \"$company_name\" ($company_id)\n");
 
-                \WP_CLI::log("Company: $company_name ($company_id)");
-                \WP_CLI::log("User ID: $user_id");
-                \WP_CLI::log("User slug: $user_slug");
-                \WP_CLI::log("User data:");
-                foreach ($user_meta as $meta_key => $meta_values) {
-                    $meta_value = implode(' | ', $meta_values);
-                    \WP_CLI::log("    [$meta_key] => $meta_value");
+                foreach ($user_ids as $user_id) {
+                    $user = get_user_by('ID', $user_id);
+                    $user_meta = get_user_meta($user_id);
+                    $user_slug = $user->user_login;
+
+                    ksort($user_meta);
+
+                    \WP_CLI::log("User ID: $user_id");
+                    \WP_CLI::log("User slug: $user_slug");
+                    \WP_CLI::log("User data:");
+                    foreach ($user_meta as $meta_key => $meta_values) {
+                        $meta_value = implode(' | ', $meta_values);
+                        \WP_CLI::log("    [$meta_key] => $meta_value");
+                    }
+                    \WP_CLI::log("");
                 }
-                \WP_CLI::log("");
             }
         }
     }
