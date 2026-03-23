b0ad3a67e5e1230accc99daa299bddbca06aec7e
leopiccionia <leopiccionia@gmail.com>
2026-03-23 10:14:06 -0300

    Temporarily disable new forms event

--- themes/hacklab-theme/single-evento.php
+++ themes/hacklab-theme/single-evento.php
@@ -220,11 +220,14 @@ if( isset($recurrence['rules']) ) {
             echo do_shortcode('[ethosGeraCertificado2]');
         } else {
             echo do_shortcode('[ethosDadosEvento menu_order=""]');
+            echo do_shortcode('[ethosDadosEventoCorpo tp_evt=""]');
+            /*
             if (hacklabr\is_paid_event($post_id) || hacklabr\is_legacy_event($post_id)) {
                 echo do_shortcode('[ethosDadosEventoCorpo tp_evt=""]');
             } else {
                 echo do_shortcode('[event-registration]');
             }
+            */
         }
     }
     ?>
